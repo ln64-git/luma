654a87cdfbb87eab967d86314759053aa761269f
Luke Anthony <ln64.ohio@gmail.com>
2025-04-13 16:33:17 -0400

    factored out settings and modal

---
+++ .obsidian/plugins/obsidian-sample-plugin/src/settings.ts
@@ -0,0 +1,36 @@
+// settings.ts
+import { App, PluginSettingTab, Setting } from 'obsidian';
+import type MyPlugin from './main';
+
+export interface LumaSettings {
+  mySetting: string;
+}
+
+export const DEFAULT_SETTINGS: LumaSettings = {
+  mySetting: 'default'
+};
+
+export class SampleSettingTab extends PluginSettingTab {
+  plugin: MyPlugin;
+
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
+
+    new Setting(containerEl)
+      .setName('Setting #1')
+      .setDesc('It\'s a secret')
+      .addText(text => text
+        .setPlaceholder('Enter your secret')
+        .setValue(this.plugin.settings.mySetting)
+        .onChange(async (value) => {
+          this.plugin.settings.mySetting = value;
+          await this.plugin.saveSettings();
+        }));
+  }
+}
