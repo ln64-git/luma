4c5133c9b5bf11e2d1965dd6143b4f1638707432
Luke Anthony <ln64.ohio@gmail.com>
2025-05-06 17:34:58 -0400

    about to process content

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { Plugin, } from 'obsidian';
+import runDailyNoteSynthesis from './commands/run-daily-note-synthesis';
+
+// Remember to rename these classes and interfaces!
+
+interface MyPluginSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: 'default'
+}
+
+export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		// This adds a simple command that can be triggered anywhere
+		this.addCommand({
+			id: 'run-daily-note-synthesis',
+			name: 'Run Daily Note Synthesis',
+			callback: () => {
+				runDailyNoteSynthesis(this.app);
+			}
+		});
+
+	}
+
+	onunload() {
+		runDailyNoteSynthesis(this.app);
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
